746f0da2970fe23a8a23647681a567a6bacd4633
vkazmirchuk <volodymyr.kazmirchuk@student.unimelb.edu.au>
2024-06-25 20:29:57 +1000

    Added input outline

--- main.cpp
+++ main.cpp
@@ -10,9 +10,26 @@
 /*
  * Libraries
  */
-using namespace std;
 
-int main() {
+#include <iostream>
+#include "input.h"
+
+int main () {
+
+    // setup configs and constants such as:
+    //   model parameters, number of images in folder, etc
+    const int NUM_IMAGES = 5;
+
+    // init model
+    read_weights ("weights_and_biases.txt");
+
+    // process each image
+    
+    for (int i = 0; i < NUM_IMAGES; i ++) {
+        read_bitmap ("img" + std::to_string (i) + ".bmp");
+    }
+
+    // free used resources (if any)
 
     return 1;
 }
