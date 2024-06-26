b69e721ff5261773e4cf9535e0140eb72f0b63f1
Peter Lu <peterhoulinlu@gmail.com>
2024-06-26 21:44:15 +1000

    some warnings suppressed

--- src/activations.cpp
+++ src/activations.cpp
@@ -10,12 +10,12 @@ vector<long double> softmax(vector<long double> input) {
     long double exp_sum = 0;
 
     // sum exponentials
-    for (int i=0; i<input.size(); i++) {
+    for (int i=0; i<(int)input.size(); i++) {
         exp_sum += exp(input[i]);
     }
 
     // calculating output softmax array
-    for (int i=0; i<input.size(); i++) {
+    for (int i=0; i<(int)input.size(); i++) {
         output.push_back (exp (input[i]) / exp_sum);
     }
 
--- src/activations.h
+++ src/activations.h
@@ -4,7 +4,7 @@
 
 using namespace std;
 // softmax declaration, function takes layer of nodes, computes softmax and outputs an array with corresponding activations
-vector<long double> softmax(vector<long double> input, int length);
+vector<long double> softmax(vector<long double> input);
 
 // rectified linear declaration
 vector<long double> reLU(vector<long double> input);
