bd520a02b1ec41d56a3a25392644c41021864ba1
DustinZoom <SinglEcho@outlook.com>
2024-06-30 21:11:48 +1000

    add params.h

---
+++ src/params.h
@@ -0,0 +1,14 @@
+#ifndef PARAMS_H
+#define PARAMS_H
+
+#define LAYERS 7
+#define INPUT 225
+#define L1 98
+#define L2 65
+#define L3 50
+#define L4 30
+#define L5 25
+#define L6 40
+#define L7 52
+
+#endif // PARAMS_H
