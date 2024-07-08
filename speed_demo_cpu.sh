8d0cf3cd3a2d755f3d21459d5fb5889b410d6b64
CarlvinceTan <137462900+CarlvinceTan@users.noreply.github.com>
2024-07-08 14:25:26 +1000

    Modified shell files

--- speed_demo_cpu.sh
+++ speed_demo_cpu.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: speed_cpu <relative_path_to_weights_and_biases.txt> <relative_path_to_input_tensor_directory>"
-    exit 1
-fi
+binary="bin/speed_cpu"
 
-weights_and_biases=$1
-input_tensor_dir=$2
-
-binary="speed_cpu"
-
-if [ ! -f "$binary" ]; then
+if [ ! -x "$binary" ]; then
     echo "Binary $binary not found!"
     exit 1
 fi
 
 start_time=$(date +%s)
-./$binary "$weights_and_biases" "$input_tensor_dir"
-
+./$binary
 end_time=$(date +%s)
+
 execution_time=$((end_time - start_time))
 
 if [ ! -f "results.csv" ]; then
--- speed_demo_gpu.sh
+++ speed_demo_gpu.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: speed_gpu <relative_path_to_weights_and_biases.txt> <relative_path_to_input_tensor_directory>"
-    exit 1
-fi
-
-weights_and_biases=$1
-input_tensor_dir=$2
-
-binary="speed_gpu"
+binary="bin/speed_gpu"
 
 if [ ! -f "$binary" ]; then
     echo "Binary $binary not found!"
@@ -16,9 +8,9 @@ if [ ! -f "$binary" ]; then
 fi
 
 start_time=$(date +%s)
-./$binary "$weights_and_biases" "$input_tensor_dir"
-
+./$binary
 end_time=$(date +%s)
+
 execution_time=$((end_time - start_time))
 
 if [ ! -f "results.csv" ]; then
